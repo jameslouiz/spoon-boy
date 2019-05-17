6bc54da0f6022f55837be191fa26ff5a6352ac54
james <james.tommo@yahoo.co.uk>
2019-05-17 16:27:19 +0100

    Basic scalar, elementwise and matrix operations

--- index.js
+++ index.js
@@ -1,3 +1,210 @@
-class Matrix {}
+const buildMatrix = (rows = 2, cols = 2) => {
+  return Array(rows).fill(Array(cols).fill(0));
+};
+
+const isFunction = functionToCheck => {
+  return (
+    functionToCheck && {}.toString.call(functionToCheck) === "[object Function]"
+  );
+};
+
+const isNaNGuard = n => {
+  if (isNaN(n)) {
+    throw new Error(`${n} is not a number`);
+  }
+};
+
+class Matrix {
+  constructor(rows = 2, cols = 2) {
+    this.cols = cols;
+    this.rows = rows;
+    this.data = buildMatrix(rows, cols);
+    this.size = cols * rows;
+  }
+
+  static add(a, b) {
+    if (!a instanceof Matrix || !b instanceof Matrix) {
+      throw new Error(`${a} or ${b} are not instances of Matrix`);
+    }
+
+    if (a.cols !== b.cols || a.rows !== b.rows) {
+      throw new Error(
+        `${a} or ${b} are not the same shape. A: [${a.rows}, ${a.cols}], B: [${
+          b.rows
+        }, ${b.cols}]`
+      );
+    }
+
+    const c = new Matrix(a.rows, a.cols);
+    const aMatrix = a.data;
+    const bMatrix = b.data;
+
+    c.map((value, i, j) => {
+      return aMatrix[i][j] + bMatrix[i][j];
+    });
+
+    return c;
+  }
+
+  static subtract(a, b) {
+    if (!a instanceof Matrix || !b instanceof Matrix) {
+      throw new Error(`${a} or ${b} are not instances of Matrix`);
+    }
+
+    if (a.cols !== b.cols || a.rows !== b.rows) {
+      throw new Error(
+        `${a} or ${b} are not the same shape. A: [${a.rows}, ${a.cols}], B: [${
+          b.rows
+        }, ${b.cols}]`
+      );
+    }
+
+    const c = new Matrix(a.rows, a.cols);
+    const aMatrix = a.data;
+    const bMatrix = b.data;
+
+    c.map((value, i, j) => {
+      return aMatrix[i][j] - bMatrix[i][j];
+    });
+
+    return c;
+  }
+
+  static divide(a, b) {
+    if (!a instanceof Matrix || !b instanceof Matrix) {
+      throw new Error(`${a} or ${b} are not instances of Matrix`);
+    }
+
+    if (a.cols !== b.cols || a.rows !== b.rows) {
+      throw new Error(
+        `${a} or ${b} are not the same shape. A: [${a.rows}, ${a.cols}], B: [${
+          b.rows
+        }, ${b.cols}]`
+      );
+    }
+
+    const c = new Matrix(a.rows, a.cols);
+    const aMatrix = a.data;
+    const bMatrix = b.data;
+
+    c.map((value, i, j) => {
+      return aMatrix[i][j] / bMatrix[i][j];
+    });
+
+    return c;
+  }
+
+  static multiply(a, b) {
+    if (!a instanceof Matrix || !b instanceof Matrix) {
+      throw new Error(`${a} or ${b} are not instances of Matrix`);
+    }
+
+    if (a.cols !== b.cols || a.rows !== b.rows) {
+      throw new Error(
+        `${a} or ${b} are not the same shape. A: [${a.rows}, ${a.cols}], B: [${
+          b.rows
+        }, ${b.cols}]`
+      );
+    }
+
+    const c = new Matrix(a.rows, a.cols);
+    const aMatrix = a.data;
+    const bMatrix = b.data;
+
+    c.map((value, i, j) => {
+      return aMatrix[i][j] * bMatrix[i][j];
+    });
+
+    return c;
+  }
+
+  static dot(a, b) {
+    if (!a instanceof Matrix || !b instanceof Matrix) {
+      throw new Error(`${a} or ${b} are not instances of Matrix`);
+    }
+
+    if (a.cols !== b.rows) {
+      throw new Error(
+        `${a} cols is not equal to ${b} cols. A: [${a.rows}, ${a.cols}], B: [${
+          b.rows
+        }, ${b.cols}]`
+      );
+    }
+
+    const c = new Matrix(a.rows, b.cols);
+    const A = a.data;
+    const B = b.data;
+
+    c.map((value, i, j) => {
+      return A[i].reduce(
+        (acc, cur, k) => {
+          acc.sum += cur * acc.b[k][acc.j];
+
+          return acc;
+        },
+        {
+          b: B,
+          sum: 0,
+          j
+        }
+      ).sum;
+    });
+
+    return c;
+  }
+
+  randomize() {
+    this.data = randomize(this.data);
+  }
+
+  add(n) {
+    if (isNaN(n)) {
+      throw new Error(`${n} is not a number`);
+    }
+    this.data = this.data.map(col => {
+      return col.map(cell => cell + n);
+    });
+  }
+
+  subtract(n) {
+    if (isNaN(n)) {
+      throw new Error(`${n} is not a number`);
+    }
+    this.data = this.data.map(col => {
+      return col.map(cell => cell - n);
+    });
+  }
+
+  divide(n) {
+    if (isNaN(n)) {
+      throw new Error(`${n} is not a number`);
+    }
+    this.data = this.data.map(col => {
+      return col.map(cell => cell / n);
+    });
+  }
+
+  multiply(n) {
+    if (isNaN(n)) {
+      throw new Error(`${n} is not a number`);
+    }
+    this.data = this.data.map(col => {
+      return col.map(cell => cell * n);
+    });
+  }
+
+  map(fn) {
+    if (!isFunction(fn)) {
+      throw new Error(`${fn} is not a function`);
+    }
+    let sum = 0;
+    this.data = this.data.map((row, i) => {
+      return row.map((col, j) => {
+        sum++;
+        return fn(col, i, j, sum - 1);
+      });
+    });
+  }
+}
 
 export default Matrix;
--- test.js
+++ test.js
@@ -1,9 +1,310 @@
 import Matrix from "./index";
 
+const toArray = matrix => {
+  return matrix.reduce((acc, row) => {
+    row.forEach(col => acc.push(col));
+    return acc;
+  }, []);
+};
+
 describe("Matrix Class", () => {
   it("Should create and instance of the matrix class", () => {
     const matrix = new Matrix();
 
     expect(matrix instanceof Matrix);
+  });
+
+  it("Should create a matrix of the correct shape", () => {
+    const cols = 4;
+    const rows = 5;
+    const matrix = new Matrix(rows, cols);
+
+    expect(matrix.cols).toBe(cols);
+    expect(matrix.rows).toBe(rows);
+    expect(matrix.data).toHaveLength(rows);
+    expect(matrix.data[0]).toHaveLength(cols);
+  });
+
+  describe("Scalar operations", () => {
+    describe("Add", () => {
+      it("Should perform scalar add to matrix", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const add = 5;
+
+        matrix.add(add);
+
+        toArray(matrix.data).forEach(n => {
+          expect(n).toBe(add);
+        });
+      });
+
+      it("Should throw if argument is not a number", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const add = "not a number";
+        const fn = () => matrix.add(add);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Subtract", () => {
+      it("Should perform scalar subtract to matrix", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const subtract = 5;
+
+        matrix.subtract(subtract);
+
+        toArray(matrix.data).forEach(n => {
+          expect(n).toBe(0 - subtract);
+        });
+      });
+
+      it("Should throw if argument is not a number", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const subtract = "not a number";
+        const fn = () => matrix.subtract(subtract);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Division", () => {
+      it("Should perform scalar division to matrix", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const divideBy = 5;
+        const add = 20;
+
+        matrix.add(add);
+        matrix.divide(divideBy);
+
+        toArray(matrix.data).forEach(n => {
+          expect(n).toBe(add / divideBy);
+        });
+      });
+
+      it("Should throw if argument is not a number", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const division = "not a number";
+        const fn = () => matrix.divide(division);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Multiplication", () => {
+      it("Should perform scalar multiplication to matrix", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const multiplyBy = 5;
+        const add = 20;
+
+        matrix.add(add);
+        matrix.multiply(multiplyBy);
+
+        toArray(matrix.data).forEach(n => {
+          expect(n).toBe(add * multiplyBy);
+        });
+      });
+
+      it("Should throw if argument is not a number", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const muliplier = "not a number";
+        const fn = () => matrix.multiply(muliplier);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Map", () => {
+      it("Should map the function to every value in the matrix", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const newValue = 99;
+
+        matrix.map(n => newValue);
+
+        toArray(matrix.data).forEach(n => {
+          expect(n).toBe(newValue);
+        });
+      });
+
+      it("Should throw if argument is not a function", () => {
+        const cols = 4;
+        const rows = 4;
+        const matrix = new Matrix(cols, rows);
+        const muliplier = "not a function";
+        const fn = () => matrix.map(muliplier);
+
+        expect(fn).toThrow();
+      });
+    });
+  });
+
+  describe("Elementwise operations", () => {
+    describe("Add", () => {
+      it("Should add each element of matrix A to each element of matrix B", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 2);
+        const base = 5;
+
+        A.add(base);
+        B.add(base);
+
+        const C = Matrix.add(A, B);
+
+        toArray(C.data).forEach(n => {
+          expect(n).toBe(base + base);
+        });
+      });
+
+      it("Should throw if A or B are not instance of Matrix", () => {
+        const A = new Matrix(3, 2);
+        const B = [];
+        const fn = () => Matrix.add(A, B);
+
+        expect(fn).toThrow();
+      });
+
+      it("Should throw if A or B are not the same shape", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 6);
+        const fn = () => Matrix.add(A, B);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Subtract", () => {
+      it("Should subtract each element of matrix A from each element of matrix B", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 2);
+        const base = 5;
+
+        A.add(base);
+        B.add(base);
+
+        const C = Matrix.subtract(A, B);
+
+        toArray(C.data).forEach(n => {
+          expect(n).toBe(base - base);
+        });
+      });
+
+      it("Should throw if A or B are not instance of Matrix", () => {
+        const A = new Matrix(3, 2);
+        const B = [];
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+
+      it("Should throw if A or B are not the same shape", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 6);
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Divide", () => {
+      it("Should divide each element of matrix A from each element of matrix B", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 2);
+        const base = 5;
+
+        A.add(base);
+        B.add(base);
+
+        const C = Matrix.divide(A, B);
+
+        toArray(C.data).forEach(n => {
+          expect(n).toBe(base / base);
+        });
+      });
+
+      it("Should throw if A or B are not instance of Matrix", () => {
+        const A = new Matrix(3, 2);
+        const B = [];
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+
+      it("Should throw if A or B are not the same shape", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 6);
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+    });
+
+    describe("Multiply", () => {
+      it("Should multiply each element of matrix A with each element of matrix B", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 2);
+        const base = 5;
+
+        A.add(base);
+        B.add(base);
+
+        const C = Matrix.multiply(A, B);
+
+        toArray(C.data).forEach(n => {
+          expect(n).toBe(base * base);
+        });
+      });
+
+      it("Should throw if A or B are not instance of Matrix", () => {
+        const A = new Matrix(3, 2);
+        const B = [];
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+
+      it("Should throw if A or B are not the same shape", () => {
+        const A = new Matrix(3, 2);
+        const B = new Matrix(3, 6);
+        const fn = () => Matrix.subtract(A, B);
+
+        expect(fn).toThrow();
+      });
+    });
+  });
+
+  describe("Matrice operations", () => {
+    describe("Dot", () => {
+      it("Should return a new matrix C that is the product of 2 Matrices", () => {
+        const A = new Matrix(2, 3);
+        const B = new Matrix(3, 2);
+
+        A.add(20);
+        B.add(20);
+
+        const C = Matrix.dot(A, B);
+
+        toArray(C.data).forEach(n => {
+          expect(n).toBe(1200);
+        });
+      });
+    });
   });
 });
